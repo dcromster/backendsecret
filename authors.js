20967a94a91d7274cd629036890623bc0e6ddd5c
Vladimir Starkov <iamstarkov@gmail.com>
2015-07-20 00:18:18 +0200

    publish @4gophers

--- authors.js
+++ authors.js
@@ -1,6 +1,6 @@
 export default [
   // post: false
-  { username: '4gophers',     start: '13 jule 2015', first: '620509320183050240', post: false },
+  { username: '4gophers',     start: '13 jule 2015', first: '620509320183050240' },
   { username: 'zkonstantin',  start: '06 jule 2015', first: '617783792443486208' },
   { username: 'nmishin',      start: '29 june 2015', first: '615292664888561664' },
 ];
