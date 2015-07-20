f69ef14b8ac4fa1f39fa591512770707c18a99fb
Vladimir Starkov <vladimir.starkov@nordnet.se>
2015-07-20 10:19:08 +0200

    add @_bugov

--- authors.js
+++ authors.js
@@ -1,5 +1,6 @@
 export default [
   // post: false
+  { username: '_bugov',       start: '13 jule 2015', first: '622959634194767872', post: false },
   { username: '4gophers',     start: '13 jule 2015', first: '620509320183050240' },
   { username: 'zkonstantin',  start: '06 jule 2015', first: '617783792443486208' },
   { username: 'nmishin',      start: '29 june 2015', first: '615292664888561664' },
