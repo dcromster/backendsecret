2bc69d20a1629d61115bbe108b4f3343cc7c3fe6
Vladimir Starkov <iamstarkov@users.noreply.github.com>
2015-07-23 14:13:54 +0200

    Update authors.js

--- authors.js
+++ authors.js
@@ -1,7 +1,7 @@
 export default [
   // post: false
-  { username: '_bugov',       start: '13 jule 2015', first: '622959634194767872', post: false },
-  { username: '4gophers',     start: '13 jule 2015', first: '620509320183050240' },
-  { username: 'zkonstantin',  start: '06 jule 2015', first: '617783792443486208' },
+  { username: '_bugov',       start: '20 july 2015', first: '622959634194767872', post: false },
+  { username: '4gophers',     start: '13 july 2015', first: '620509320183050240' },
+  { username: 'zkonstantin',  start: '06 july 2015', first: '617783792443486208' },
   { username: 'nmishin',      start: '29 june 2015', first: '615292664888561664' },
 ];
