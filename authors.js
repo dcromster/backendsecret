364a355aee64da72f8342f37af6ef6d775ea4834
Vladimir Starkov <iamstarkov@gmail.com>
2015-07-27 03:36:41 +0500

    publish @_bugov

--- authors.js
+++ authors.js
@@ -1,6 +1,6 @@
 export default [
   // post: false
-  { username: '_bugov',       start: '20 july 2015', first: '622959634194767872', post: false },
+  { username: '_bugov',       start: '20 july 2015', first: '622959634194767872' },
   { username: '4gophers',     start: '13 july 2015', first: '620509320183050240' },
   { username: 'zkonstantin',  start: '06 july 2015', first: '617783792443486208' },
   { username: 'nmishin',      start: '29 june 2015', first: '615292664888561664' },
