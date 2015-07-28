7f855ec3c1081338f25d7fb90fc40a762bf75675
Vladimir Starkov <vladimir.starkov@nordnet.se>
2015-07-28 11:51:05 +0200

    add @KSDaemon

--- authors.js
+++ authors.js
@@ -1,5 +1,6 @@
 export default [
   // post: false
+  { username: 'KSDaemon',     start: '27 jule 2015', first: '625595316365062144', post: false },
   { username: '_bugov',       start: '20 july 2015', first: '622959634194767872' },
   { username: '4gophers',     start: '13 july 2015', first: '620509320183050240' },
   { username: 'zkonstantin',  start: '06 july 2015', first: '617783792443486208' },
