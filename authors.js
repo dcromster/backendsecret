ce80c637b0b6de9cb831a9b416715f638f3a12eb
Vladimir Starkov <iamstarkov@users.noreply.github.com>
2015-08-17 00:50:38 +0200

    publish @dsimonov

--- authors.js
+++ authors.js
@@ -1,6 +1,6 @@
 export default [
   // post: false
-  { username: 'dsimonov',     start: '10 aug 2015', first: '630582718598791168', post: false },
+  { username: 'dsimonov',     start: '10 aug 2015', first: '630582718598791168' },
   { username: 'lecharton',    start: '03 aug 2015', first: '627961841357049856' },
   { username: 'KSDaemon',     start: '27 jul 2015', first: '625595316365062144' },
   { username: '_bugov',       start: '20 jul 2015', first: '622959634194767872' },
