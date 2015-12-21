e072c43c472f88305bff436cece0dd6638cee519
Roman Milovskiy <dc-rom@mail.ru>
2015-12-21 11:11:47 +0300

    Blohin start

--- authors.js
+++ authors.js
@@ -1,8 +1,8 @@
 export default [
   // post: false
   // { username: 'twitter_name',      start: '07 sep 2015', first: '640816652293144576', post: false }, 
-  // { username: 'Blohin',      start: '21 dec 2015', first: '', post: false }, 
-    { username: 'axique',      start: '14 dec 2015', first: '676316898255110144'}, 
+  { username: 'Blohin',      start: '21 dec 2015', first: '678842716055314432', post: false }, 
+  { username: 'axique',      start: '14 dec 2015', first: '676316898255110144'}, 
   { username: 'M0sth8',      start: '07 dec 2015', first: '673858081277796352' }, 
   { username: 'heapyhop',      start: '30 nov 2015', first: '671281337555410944' }, 
   { username: 'lambdadmitry',      start: '23 nov 2015', first: '670194583100383232' }, 
