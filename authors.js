3a3edccc902724d83e0b293ba4466579a2991189
Roman Milovskiy <rom@pakt.spb.ru>
2016-03-22 17:50:07 +0300

    vivaldi_ru start

--- authors.js
+++ authors.js
@@ -1,5 +1,6 @@
 export default [
   // { username: 'twitter_name', start: '15 feb 2016', first: '640816652293144576', post: false },
+   username: 'vivaldi_ru', start: '21 feb 2016', first: '711834412372070400', post: false },
   { username: 'PetrMyazin', start: '14 mar 2016', first: '709267362537717760'},
   { username: 'dronmdf', start: '07 mar 2016', first: '706739777542995968'},
   { username: 'AnnaMelekhova', start: '29 feb 2016', first: '704214722892144640' },
