24fa8d30a4a1ba743b8683d726805e7b60335928
Roman Milovskiy <dc-rom@mail.ru>
2016-05-16 18:28:50 +0300

    LK4D4math start

--- authors.js
+++ authors.js
@@ -1,6 +1,6 @@
 export default [
   // { username: 'twitter_name', start: '15 feb 2016', first: '640816652293144576', post: false },
-    // { username: 'LK4D4math', start: '16 may 2016', first: '', post: false },
+  { username: 'LK4D4math', start: '16 may 2016', first: '732193210991149057', post: false },
   { username: 'postgresmen', start: '18 apr 2016', first: '721971547859124224'},
   { username: 'servzin',    start: '11 apr 2016', first: '719460712876216320' },
   { username: 'gvsmirnov',       start: '04 apr 2016', first: '716861849850015744'},
