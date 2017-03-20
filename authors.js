12da43cbd3b0e79872a22d05602a54739fa3fc80
Roman Milovskiy <dcromster@gmail.com>
2017-03-20 09:44:11 +0300

    xors_ start

--- authors.js
+++ authors.js
@@ -1,7 +1,8 @@
 export default [
-  // { username: 'twitter_name', start: '15 feb 2017', first: '640816652293144576', post: false},
+  // { username: 'twitter_name', start: '15 mar 2017', first: '640816652293144576', post: false },
+  { username: 'xors_',           start: '20 mar 2017', first: '843709048969187328', post: false },
   { username: 'en_austin-2',     start: '13 mar 2017', first: '841218564795314176' },
-  { username: 'torgeek',         start: '06 mar 2017', first: '838736704320204800'}, 
+  { username: 'torgeek',         start: '06 mar 2017', first: '838736704320204800' }, 
   { username: 'shutyourprogram', start: '27 feb 2017', first: '836131434955948032' },
   { username: 'webdeva',         start: '20 feb 2017', first: '833539291531866113' },
   { username: 'alexkorotkikh',   start: '06 feb 2017', first: '828588323765678080' },
