13a812ba0c504fd7c8da138b7ff1fa20e9d763be
Roman Milovskiy <dcromster@gmail.com>
2017-05-08 10:43:34 +0300

    a_abashev end

--- authors.js
+++ authors.js
@@ -1,6 +1,6 @@
 export default [
   // { username: 'twitter_name', start: '15 mar 2017', first: '640816652293144576', post: false },
-  { username: 'a_abashev',       start: '01 maн 2017', first: '858966452560441344', post: false },
+  { username: 'a_abashev',       start: '01 maн 2017', first: '858966452560441344' },
   { username: 'CSharpGL',        start: '17 apr 2017', first: '853838354059841536' },
   { username: 'freiksenet_ru',   start: '10 apr 2017', first: '851367755395846144' },
   { username: '_beargummy',      start: '27 mar 2017', first: '846326710727593985' },
