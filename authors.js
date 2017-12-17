3f01a61f1e7a174b520c7a96f36f1708c69a31ae
Roman Milovskiy <dcromster@gmail.com>
2017-12-17 22:31:18 +0300

    yegor256 end

--- authors.js
+++ authors.js
@@ -1,6 +1,6 @@
 export default [
   // { username: 'twitter_name', start: '15 mar 2017', first: '640816652293144576', post: false },
-  { username: 'yegor256', start: '11 dec 2017', first: '939929557653708801', post: false },
+  { username: 'yegor256',        start: '11 dec 2017', first: '939929557653708801' },
   { username: 'a_soldatenko',    start: '04 dec 2017', first: '937608477421301760' },
   { username: 'tolkv',           start: '13 nov 2017', first: '930828812102840327' },
   { username: 'svetlyak40wt',    start: '06 nov 2017', first: '927466036961693697' },
