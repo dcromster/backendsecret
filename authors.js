1180d63f00af7345033ee46f406d8d61c7d81d35
Roman Milovskiy <dcromster@gmail.com>
2019-01-07 11:38:14 +0300

    sam_dark end

--- authors.js
+++ authors.js
@@ -1,6 +1,6 @@
 export default [
   // { username: 'twitter_name', start: '15 feb 2018', first: '640816652293144576', post: false },
-  { username: 'sam_dark',        start: '24 dec 2018', first: '1077109862444777472', post: false },
+  { username: 'sam_dark',        start: '24 dec 2018', first: '1077109862444777472' },
   { username: 'FelixTheBest',    start: '17 dec 2018', first: '1074619686279618560' },
   { username: 'vgdnet',          start: '10 dec 2018', first: '1072018012473974784' },
   { username: 'binakot',         start: '03 dec 2018', first: '1069492850063286273' },
